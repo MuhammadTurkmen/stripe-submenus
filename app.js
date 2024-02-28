595c52f79aeafa1df20322269c1c7a926bc54554
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-02-28 20:18:29 +0430

    init

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ sidebar.innerHTML = sublinks.map((item) => {
     const {links, page} = item
     return `<article>
         <h4>${page}</h4>  
-        <div class="sidebar-sublinks">  
+        <div class="sidebar-sublinks">   
             ${links.map((item) => {  
                 return `<a href="${item.url}"></a>`     
             }).join('')}  
