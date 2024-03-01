487a8208e4ebc95aec681e0e1f7807cea4cbfd29
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-01 20:14:13 +0430

    init

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ sidebar.innerHTML = sublinks.map((item) => {
         <div class="sidebar-sublinks"> 
             ${links.map((item) => {  
                 return `<a href="${item.url}"></a>`   
-            }).join('')} 
+            }).join('')}  
         </div>  
     </article>`
 }).join('') 
