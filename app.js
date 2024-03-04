b8f5e42a77960ac454899b4b643b4b8929cd19a4
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-04 19:26:05 +0430

    init

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ import sublinks from './data.js'
 
 const toggleBtn = document.querySelector('.toggle-btn')
 const closeBtn = document.querySelector('.close-btn')
-const toggleBtn = document.querySelector('.toggle-btn')
+const sidebarWrapper = document.querySelector('.sidebar-wrapper')
 const toggleBtn = document.querySelector('.toggle-btn')
