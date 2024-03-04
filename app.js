987876194d5499e38902ef6e11ac946db6713d3f
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-04 19:34:03 +0430

    init

--- app.js
+++ app.js
@@ -14,3 +14,6 @@ const nav = document.querySelector('.nav')
 toggleBtn.addEventListener('click', () => {
     sidebarWrapper.classList.add('show')
 })
+closeBtn.addEventListener('click', () => {
+    sidebarWrapper.classList.remove('show')
+})
