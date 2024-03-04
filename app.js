d855ee4646d39de0ecdf86981facad2eb11917d9
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-04 19:24:29 +0430

    init

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
 import sublinks from './data.js'
 
 const toggleBtn = document.querySelector('.toggle-btn')
+const toggleBtn = document.querySelector('.toggle-btn')
+const toggleBtn = document.querySelector('.toggle-btn')
+const toggleBtn = document.querySelector('.toggle-btn')
