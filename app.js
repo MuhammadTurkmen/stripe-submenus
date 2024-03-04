7cf2daf92fc5eadd3c516b7b9a66d7ecf822b8e8
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-04 19:28:20 +0430

    init

--- app.js
+++ app.js
@@ -4,3 +4,5 @@ const toggleBtn = document.querySelector('.toggle-btn')
 const closeBtn = document.querySelector('.close-btn')
 const sidebarWrapper = document.querySelector('.sidebar-wrapper')
 const sidebar = document.querySelector('.sidebar-links')
+
+const linkBtns = document.querySelectorAll('.linke-btn')
