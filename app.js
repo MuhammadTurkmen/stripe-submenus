1a6f757bacd3be714dd6cab5b449a4c4f7e04227
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-05 11:17:41 +0430

    init

--- app.js
+++ app.js
@@ -69,6 +69,7 @@ hero.addEventListener('mouseover', function(e) {
     submenu.classList.remove('show')
 })
 nav.addEventListener('mouseover', function(e) {
-    if(!e.target.classList.contains('link-btn'))
-    submenu.classList.remove('show')
+    if(!e.target.classList.contains('link-btn')) {
+        submenu.classList.remove('show')
+    }
 })
